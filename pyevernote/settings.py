f04d66aa093c9f965fa4ad6229376b6f9a2d4af0
daipeng <daipeng123456@gmail.com>
2013-11-25 16:21:52 +0800

    fix

--- pyevernote/settings.py
+++ pyevernote/settings.py
@@ -12,8 +12,12 @@ PASSWORD = '123456'
 # evernote 测试服务器地址，可以去这里设置一些测试用数据
 EVERNOTE_SANDBOX_URL = 'https://sandbox.evernote.com'
 
+# use for the user to set the default value
+# 默认的evernote存储目录
+EVERNOTE_DIR = "/home/daipeng/Desktop/program/evernote"
+# 默认的扩展名
+DEFAULT_EXT = ".org"
 
 # test guid
-
 NOTEBOOK_GUID_1 = '6ccaa7d7-cbdb-4190-8c28-121e603f0b8c'
 NOTE_GUID_1 = 'd4b69402-5d4f-4636-8978-4246841cdb88'
--- pyevernote/shownote.py
+++ pyevernote/shownote.py
@@ -2,26 +2,44 @@
 # encoding: utf-8
 
 import ConfigParser
+from optparse import OptionParser, OptionValueError
 from evernote.api.client import EvernoteClient
 from evernote.edam.notestore import ttypes
+
 import settings
 
 
-def shownote():
+def shownote(args, file_name=None):
     client = EvernoteClient(token=settings.TOKEN)
     noteStore = client.get_note_store()
 
-    notefilter = ttypes.NoteFilter(notebookGuid=settings.NOTEBOOK_GUID_1)
-    offset = 0
-    limit = 200
-    note  =  noteStore.getNote(settings.NOTE_GUID_1, True, True, False, False)
-    print note.guid
-    print note.title
-    print note.content
-    print note.created
-    print note.updated
-    print note.notebookGuid
+    note_guid = args[0]
+    note  =  noteStore.getNote(note_guid, True, True, False, False)
+    print "note_guid:",note.guid
+    print "title:",note.title
+    print "create_time:",note.created
+    print "update_time:",note.updated
+    print "notebook_guid:",note.notebookGuid
+    content = note.content
+    if file_name:
+        with open(file_name, 'w') as f:
+            f.write(note.content)
+    else:
+        print "========== content ========="
+        print note.content
+
+
+def parse_args():
+    usage = "usage: %prog [options] note_guid"
+    parser = OptionParser(usage=usage)
+    parser.add_option("-f", "--file", dest="filename",
+                      help="write the note content to file, need combine with -a", metavar="FILE")
+    options, args = parser.parse_args()
+    if len(args) < 1:
+        raise OptionValueError("must give the note guid.")
+    shownote(args, options.filename)
 
 
 if __name__ == '__main__':
-    shownote()
+    # shownote()
+    parse_args()
