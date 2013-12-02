52677be0c8fb64c4c45195571adc9d69a69f79bd
daipeng <daipeng123456@gmail.com>
2013-12-02 17:24:12 +0800

    fix

--- pyevernote/listnotebooks.py
+++ pyevernote/listnotebooks.py
@@ -3,6 +3,8 @@
 
 import settings
 import ConfigParser
+import string
+import time
 from evernote.api.client import EvernoteClient
 from evernote.edam.notestore import ttypes
 
@@ -13,13 +15,12 @@ def listnotebooks():
 
     notebook_list = notestore.listNotebooks()
 
+    print string.ljust("notebook_guid", 50), string.ljust("notebook_name", 30), string.ljust("notebook_create_time", 12)
+    print '-' * 100
     for notebook in notebook_list:
-        print '============'
-        print notebook.guid
-        print notebook.name
-        print notebook.defaultNotebook
-        print notebook.serviceCreated
-        print notebook.serviceUpdated
+        create_time = time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime(notebook.serviceCreated/1000))
+        print string.ljust(`notebook.guid`, 50), string.ljust(`notebook.name`, 30), string.ljust(create_time, 12)
+
 
 if __name__ == '__main__':
     listnotebooks()
