29d403c4b4286cea799d3b2df20ed7e7c6c24e6c
daipeng <daipeng123456@gmail.com>
2013-12-07 17:21:01 +0800

    fix

--- pyevernote/listnotes.py
+++ pyevernote/listnotes.py
@@ -4,21 +4,44 @@
 import settings
 import sys
 import time
-from optparse import OptionParser
+from optparse import OptionParser, OptionError
 from evernote.api.client import EvernoteClient
 from evernote.edam.notestore import ttypes
 
 
+def listnotes_options():
+    parser = OptionParser()
+    parser.add_option("-B", "--nbg", dest="nbg",
+                      help="set the notebook guid to list",
+                      metavar="NoteBookGuid")
+    parser.add_option("-b", "--nbn", dest="nbn",
+                      help="set the notebook name to list, cover the --nbg")
+    parser.add_option("-T", "--tg", dest="tg",
+                      help="set the tag guid to list",
+                      metavar="TagGuid")
+    parser.add_option("-t", "--tn", dest="tn",
+                      help="set the tag name to list, cover the --tn",
+                      metavar="TagName")
+    # parser.add_option('-p', "--page", dest="page", default=1, type="int",
+    #                   help="set the page to list, default  1",
+    #                   metavar="Page")
+    # parser.add_option("-s", "-pagesize", dest="page_size", default=20, type="int",
+    #                   help="set the page size, default 20",
+    #                   metavar="PageSize")
+    return parser.parse_args()
+
+def check_options(options):
+    if not (options.nbg or options.nbn or options.tg or options.tn):
+        raise OptionError("must give an contidion, use -h for detail")
+
+def get_note_filter(options):
+    if options.nbn:
+        pass
+
 def listnotes():
     # the option parser
-    parser = OptionParser()
-    parser.add_option("-f", "--file", dest="filename",
-                      help="also write the notes list to file",
-                      metavar="FILE")
-    (options, args) = parser.parse_args()
-    if len(args) != 1:
-        print "please give the notebook guid for list notes"
-        sys.exit(-1)
+    (options, args) = listnotes_options()
+    check_options(options)
 
     client = EvernoteClient(token=settings.TOKEN)
     notestore = client.get_note_store()
